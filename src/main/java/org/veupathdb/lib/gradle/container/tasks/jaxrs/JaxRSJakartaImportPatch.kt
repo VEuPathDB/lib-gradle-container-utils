4e90df909058505a87b2a3b6a1e5f3fca3ce3db8
Elizabeth <foxcapade@gmail.com>
2022-09-30 13:46:10 -0400

    jakarta patch added

---
+++ src/main/java/org/veupathdb/lib/gradle/container/tasks/jaxrs/JaxRSJakartaImportPatch.kt
@@ -0,0 +1,74 @@
+package org.veupathdb.lib.gradle.container.tasks.jaxrs
+
+import org.veupathdb.lib.gradle.container.tasks.base.JaxRSSourceAction
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.File
+import java.util.*
+import java.util.stream.Stream
+
+open class JaxRSJakartaImportPatch : JaxRSSourceAction() {
+
+  companion object {
+    private const val ReplaceOld = "javax.ws"
+    private const val ReplaceNew = "jakarta.ws"
+
+    private const val ResourceDir = "resources"
+    private const val SupportDir = "support"
+
+    const val TaskName = "patch-javax-ws"
+  }
+
+  override val pluginDescription: String
+    get() = "Patches generated jax-rs classes to import jakarta.ws rather than javax.ws"
+
+  override fun execute() {
+    log.open()
+
+    // Get all the generated source root directories
+    getGeneratedSourceDirectories()
+      .flatMap { Stream.of(                 // Expand the stream to the 2 relevant subdirectories
+        File(it, ResourceDir),
+        File(it, SupportDir)
+      ) }
+      .filter { it.exists() }               // Filter out any erroneous paths
+      .map { it.listFiles() }               // Map to lists of files in each directory
+      .filter { it != null }                // Filter out any erroneous entries (just to be safe)
+      .flatMap { Arrays.stream(it) }        // Expand the stream to a stream of files
+      .filter { it.name.endsWith(".java") } // Filter the stream down to just java files
+      .forEach { processFile(it) }
+
+    log.close()
+  }
+
+  private fun processFile(file: File) {
+    val tmpFile = File("${file.path}.tmp")
+    tmpFile.createNewFile()
+
+    tmpFile.bufferedWriter().use { output ->
+      file.bufferedReader().use { input -> processContents(output, input) }
+      output.flush()
+    }
+
+    tmpFile.copyTo(file, true)
+    tmpFile.delete()
+  }
+
+  private fun processContents(output: BufferedWriter, input: BufferedReader) {
+    var line = input.readLine()
+
+    while (line != null) {
+      output.write(processLine(line))
+      output.newLine()
+
+      line = input.readLine()
+    }
+  }
+
+  private fun processLine(line: String) =
+    if (line.startsWith("import")) {
+      line.replace(ReplaceOld, ReplaceNew)
+    } else {
+      line
+    }
+}
